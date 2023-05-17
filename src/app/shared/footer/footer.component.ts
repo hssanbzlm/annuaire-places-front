281f8d945aff7a949e02883bb95b52f8d19087d9
Hassan Bouzlima <bouzlima.h@live.fr>
2023-05-17 15:23:04 +0100

    Implement footer design

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import {
+  faLinkedin,
+  faFacebook,
+  faGithub,
+  faTwitter,
+  faDev,
+} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faDev = faDev;
 }
