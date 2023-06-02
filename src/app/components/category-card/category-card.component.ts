e9bfdf1f66f1087728016e5b5de81b0882d20ff0
Hassan Bouzlima <bouzlima.h@live.fr>
2023-06-02 21:35:53 +0100

    Fetch categories from store and display them with carousel

--- src/app/components/category-card/category-card.component.ts
+++ src/app/components/category-card/category-card.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faShop } from '@fortawesome/free-solid-svg-icons';
+import { Category } from 'src/app/Interfaces/category';
 @Component({
   selector: 'app-category-card',
   templateUrl: './category-card.component.html',
@@ -7,4 +8,6 @@ import { faShop } from '@fortawesome/free-solid-svg-icons';
 })
 export class CategoryCardComponent {
   faShop = faShop;
+
+  @Input() category!: Category;
 }
--- src/app/pages/category-list/category-list.component.ts
+++ src/app/pages/category-list/category-list.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/Interfaces/category';
+import { AppState } from 'src/app/Store';
+import { selectCategories } from 'src/app/Store/category/categories.selectors';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css'],
 })
-export class CategoryListComponent {}
+export class CategoryListComponent {
+  categoryList$!: Observable<Category[]>;
+  itemsPerSlide = 3;
+  singleSlideOffset = true;
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.categoryList$ = this.store.select(selectCategories);
+  }
+}
