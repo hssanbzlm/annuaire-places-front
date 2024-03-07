68802fc23d995998f87cccc0a51a5283c431fbac
Hassan Bouzlima <bouzlima.h@live.fr>
2024-03-07 15:43:09 +0100

    Add countries and categories to search bar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+import { selectCountriesName } from 'src/app/Store/country/countries.selectors';
+import { selectCategoriesName } from 'src/app/Store/category/categories.selectors';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/Store';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
@@ -7,4 +12,12 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 })
 export class SearchBarComponent {
   faMagnifyingGlass = faMagnifyingGlass;
+  countries$!: Observable<string[]>;
+  categories$!: Observable<string[]>;
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.countries$ = this.store.select(selectCountriesName);
+    this.categories$ = this.store.select(selectCategoriesName);
+  }
 }
